7a260ebccb7e63dd5b50e985d9e4d67386a673e5
Steve Clark <steve.a.clark48@gmail.com>
2021-10-05 05:12:14 -0500

    playing with styling

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { Paper, Tab, Tabs } from "@material-ui/core";
-import { Link, useLocation } from "react-router-dom";
+// import React from "react";
+// import { Paper, Tab, Tabs } from "@material-ui/core";
+// import { Link, useLocation } from "react-router-dom";
 
-import styles from "./Nav.module.scss";
+// import styles from "./Nav.module.scss";
 
-const NavBar = (props) => {
-  const selected = useLocation().pathname.toUpperCase();
+// const NavBar = (props) => {
+//   const selected = useLocation().pathname.toUpperCase();
 
-  return (
-    <div className={styles.container}>
-      <Paper className={styles.navContainer}>
-        <Tabs
-          value={selected}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab component={Link} to="/" value="/" label="About"></Tab>
-          <Tab component={Link} to="/PFT" value="/PFT" label="PFT"></Tab>
-          <Tab component={Link} to="/CFT" value="/CFT" label="CFT"></Tab>
-        </Tabs>
-      </Paper>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.container}>
+//       <Paper className={styles.navContainer}>
+//         <Tabs
+//           value={selected}
+//           indicatorColor="primary"
+//           textColor="primary"
+//           centered
+//         >
+//           <Tab component={Link} to="/" value="/" label="About"></Tab>
+//           <Tab component={Link} to="/PFT" value="/PFT" label="PFT"></Tab>
+//           <Tab component={Link} to="/CFT" value="/CFT" label="CFT"></Tab>
+//         </Tabs>
+//       </Paper>
+//     </div>
+//   );
+// };
 
-export default NavBar;
+// export default NavBar;
